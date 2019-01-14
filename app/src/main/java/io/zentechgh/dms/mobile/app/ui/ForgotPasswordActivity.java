dc20c24054c22644e8a40664216084a88699d268
iCODE <alisampson2@gmail.com>
2019-01-14 12:42:29 -0800

    Scanner Library integrated successfully Last Update - 8-Jan-18

---
+++ app/src/main/java/io/zentechgh/dms/mobile/app/ui/ForgotPasswordActivity.java
@@ -0,0 +1,143 @@
+package io.zentechgh.dms.mobile.app.ui;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import io.zentechgh.dms.mobile.app.R;
+import maes.tech.intentanim.CustomIntent;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    CoordinatorLayout coordinatorLayout;
+
+    Toolbar toolbar;
+
+    TextView toolbar_title;
+
+    EditText editTextEmail;
+
+    FirebaseAuth mAuth;
+
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        // getting ref to parent layout
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+
+        // getting reference to views
+        toolbar = findViewById(R.id.toolbar);
+        toolbar_title =  findViewById(R.id.toolbar_title);
+        // setting supportActionBar to custom toolbar
+        setSupportActionBar(toolbar);
+        // setting navigation icon and onClickListener
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // finish activity
+                finish();
+            }
+        });
+
+        // getting reference to view
+        editTextEmail =  findViewById(R.id.editTextEmail);
+
+        // initializing FireBaseAuth class
+        mAuth = FirebaseAuth.getInstance();
+
+        // getting reference to view
+        progressBar = findViewById(R.id.progressBar);
+
+    }
+
+    //OnClickListener for password reset button
+    public void resetPasswordButton(View view) {
+
+        // getting text from editText field
+        String email  = editTextEmail.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            // trigger's a shaking animation to alert user of no email entered
+            YoYo.with(Techniques.Shake).playOn(editTextEmail);
+            editTextEmail.setError(getString(R.string.error_empty_field));
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            // trigger's a shaking animation to alert user of no email entered
+            YoYo.with(Techniques.Shake).playOn(editTextEmail);
+            editTextEmail.setError(getString(R.string.error_invalid_email));
+        }
+
+        if(!TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            // method call to reset password
+            resetPassword();
+        }
+
+    }
+
+    // method that send a password reset link to user's email to reset password
+    private void resetPassword(){
+
+        // displays progressBar
+        progressBar.setVisibility(View.VISIBLE);
+
+        // getting text from editText field
+        String email  = editTextEmail.getText().toString();
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    // display successful message along with instructions to reset password
+                    Snackbar.make(coordinatorLayout,getString(R.string.reset_password_instruction),5000).show();
+                }
+                else{
+                    // display message if there is an exception
+                    Snackbar.make(coordinatorLayout,task.getException().getMessage(),3000).show();
+                }
+
+                // hides the progressBar
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        // starts the Login activity
+        //startActivity(new Intent(ForgotPasswordActivity.this,SignInActivity.class));
+
+        // Add a custom animation ot the activity
+        //CustomIntent.customType(ForgotPasswordActivity.this,"fadein-to-fadeout");
+
+        // finish the activity
+        finish();
+
+    }
+
+}
---
+++ app/src/main/java/io/zentechgh/dms/mobile/app/ui/SignInActivity.java
@@ -0,0 +1,220 @@
+package io.zentechgh.dms.mobile.app.ui;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import io.zentechgh.dms.mobile.app.R;
+import io.zentechgh.dms.mobile.app.model.Users;
+import maes.tech.intentanim.CustomIntent;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ConstraintLayout constraintLayout;
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button login_button;
+    Button forgotPassword;
+
+    FirebaseAuth mAuth;
+
+    FirebaseUser user;
+
+    Users users;
+
+    DatabaseReference userRef;
+
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        constraintLayout = findViewById(R.id.constraintLayout);
+
+        // getting reference to ids of views
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        login_button =  findViewById(R.id.login_button);
+        forgotPassword = findViewById(R.id.forgot_password);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        users = new Users();
+
+        // onClickListener for loginButton
+        login_button.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.login_button:
+
+                // login process
+                inputValidation();
+
+                break;
+            case R.id.forgot_password:
+
+                // method call to open forgotPassword activity
+                forgotPassword();
+
+                break;
+        }
+
+    }
+
+    public void forgotPassword(){
+
+        // start the home activity
+        startActivity(new Intent(SignInActivity.this,ForgotPasswordActivity.class));
+
+        // Add a custom animation ot the activity
+        CustomIntent.customType(SignInActivity.this,"fadein-to-fadeout");
+
+        // finishes this activity(prevents user from going back to this activity when back button is pressed)
+        finish();
+
+    }
+
+    private void inputValidation(){
+
+        // getting text from editText fields
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            YoYo.with(Techniques.Shake).playOn(editTextEmail);
+            editTextEmail.setError(getString(R.string.error_email));
+        }
+
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            YoYo.with(Techniques.Shake).playOn(editTextEmail);
+            editTextEmail.setError(getString(R.string.error_invalid_email));
+        }
+
+        else if(TextUtils.isEmpty(password)){
+            YoYo.with(Techniques.Shake).playOn(editTextPassword);
+            editTextPassword.setError(getString(R.string.error_password));
+        }
+
+        else if(password.length() < 6){
+            YoYo.with(Techniques.Shake).playOn(editTextPassword);
+            editTextPassword.setError(getString(R.string.error_password_length));
+        }
+
+        else{loginUser();}
+    }
+
+    // method to login User
+    private void loginUser() {
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        // getting text from edtitext fields
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            checkIfEmailIsVerified();
+
+                        }
+                        else{
+                            // display a error message
+                            Snackbar.make(constraintLayout,task.getException().getMessage(),Snackbar.LENGTH_LONG).show();
+                        }
+
+                        // dismiss progressBar
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+
+    }
+
+    // method to check if email is verified
+    private void checkIfEmailIsVerified(){
+
+        user = mAuth.getCurrentUser();
+
+        boolean isEmailVerified = user.isEmailVerified();
+
+        if(isEmailVerified){
+
+            // display a successful login message
+            Toast.makeText(SignInActivity.this,getString(R.string.sign_in_successful),Toast.LENGTH_SHORT).show();
+
+            // start the home activity
+            startActivity(new Intent(SignInActivity.this,HomeActivity.class));
+
+            // Add a custom animation ot the activity
+            CustomIntent.customType(SignInActivity.this,"fadein-to-fadeout");
+
+            // finishes this activity(prevents user from going back to this activity when back button is pressed)
+            finish();
+
+            // clear the text fields
+            clearTextFields();
+
+        }
+        else{
+
+            // display message if email is not verified
+            Toast.makeText(SignInActivity.this, getString(R.string.text_email_not_verified),
+                    Toast.LENGTH_LONG).show();
+
+            // signs user out and restarts the Login Activity
+            mAuth.signOut();
+
+            // restarts the activity
+            startActivity(new Intent (SignInActivity.this,SignInSignUpActivity.class));
+
+            // Add a custom animation ot the activity
+            CustomIntent.customType(SignInActivity.this,"fadein-to-fadeout");
+
+            // finish the activity
+            finish();
+
+        }
+
+    }
+
+    // method to clear editText fields
+    private void clearTextFields(){
+        editTextEmail.setText("");
+        editTextPassword.setText("");
+    }
+
+}
